d24c39c7353eff210112b771344726fd0d5eae64
Antonio <j.antonio_analista@hotmail.com>
2023-02-01 14:04:39 -0300

    Designer changens

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.pctbAfaRussian = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lbPronuncia = new System.Windows.Forms.Label();
-            this.lbPT = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.lbContador = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbPT = new System.Windows.Forms.Label();
+            this.lbPronuncia = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pctbAfaRussian = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctbAfaRussian)).BeginInit();
             this.SuspendLayout();
@@ -63,29 +63,76 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // button1
+            // lbContador
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(76, 359);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Anterior";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.lbContador.AutoSize = true;
+            this.lbContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbContador.Location = new System.Drawing.Point(301, 285);
+            this.lbContador.Name = "lbContador";
+            this.lbContador.Size = new System.Drawing.Size(16, 24);
+            this.lbContador.TabIndex = 9;
+            this.lbContador.Text = "-";
             // 
-            // button2
+            // label5
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(270, 359);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Próximo";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(235, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Card: ";
+            // 
+            // lbPT
+            // 
+            this.lbPT.AutoSize = true;
+            this.lbPT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPT.Location = new System.Drawing.Point(167, 285);
+            this.lbPT.Name = "lbPT";
+            this.lbPT.Size = new System.Drawing.Size(16, 24);
+            this.lbPT.TabIndex = 7;
+            this.lbPT.Text = "-";
+            // 
+            // lbPronuncia
+            // 
+            this.lbPronuncia.AutoSize = true;
+            this.lbPronuncia.Enabled = false;
+            this.lbPronuncia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPronuncia.Location = new System.Drawing.Point(252, 248);
+            this.lbPronuncia.Name = "lbPronuncia";
+            this.lbPronuncia.Size = new System.Drawing.Size(16, 24);
+            this.lbPronuncia.TabIndex = 6;
+            this.lbPronuncia.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pronuncia: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Em PT: ";
+            // 
+            // pctbAfaRussian
+            // 
+            this.pctbAfaRussian.Image = global::AlfaBetoRusso.Properties.Resources._1;
+            this.pctbAfaRussian.Location = new System.Drawing.Point(50, 32);
+            this.pctbAfaRussian.Name = "pctbAfaRussian";
+            this.pctbAfaRussian.Size = new System.Drawing.Size(320, 213);
+            this.pctbAfaRussian.TabIndex = 3;
+            this.pctbAfaRussian.TabStop = false;
             // 
             // button3
             // 
@@ -99,74 +146,29 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pctbAfaRussian
+            // button2
             // 
-            this.pctbAfaRussian.Image = global::AlfaBetoRusso.Properties.Resources._1;
-            this.pctbAfaRussian.Location = new System.Drawing.Point(50, 32);
-            this.pctbAfaRussian.Name = "pctbAfaRussian";
-            this.pctbAfaRussian.Size = new System.Drawing.Size(320, 213);
-            this.pctbAfaRussian.TabIndex = 3;
-            this.pctbAfaRussian.TabStop = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(270, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Próximo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(73, 309);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 24);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Em PT: ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(72, 270);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Pronuncia: ";
-            // 
-            // lbPronuncia
-            // 
-            this.lbPronuncia.AutoSize = true;
-            this.lbPronuncia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbPronuncia.Location = new System.Drawing.Point(184, 270);
-            this.lbPronuncia.Name = "lbPronuncia";
-            this.lbPronuncia.Size = new System.Drawing.Size(16, 24);
-            this.lbPronuncia.TabIndex = 6;
-            this.lbPronuncia.Text = "-";
-            // 
-            // lbPT
-            // 
-            this.lbPT.AutoSize = true;
-            this.lbPT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbPT.Location = new System.Drawing.Point(184, 309);
-            this.lbPT.Name = "lbPT";
-            this.lbPT.Size = new System.Drawing.Size(16, 24);
-            this.lbPT.TabIndex = 7;
-            this.lbPT.Text = "-";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(239, 309);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 24);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Card: ";
-            // 
-            // lbContador
-            // 
-            this.lbContador.AutoSize = true;
-            this.lbContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbContador.Location = new System.Drawing.Point(305, 309);
-            this.lbContador.Name = "lbContador";
-            this.lbContador.Size = new System.Drawing.Size(16, 24);
-            this.lbContador.TabIndex = 9;
-            this.lbContador.Text = "-";
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(76, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Anterior";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
