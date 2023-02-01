9e91c958588231114c9e28065a32887d18fb3dd0
Antonio <j.antonio_analista@hotmail.com>
2023-02-01 13:35:34 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace AlfaBetoRusso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pctbAfaRussian = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbPronuncia = new System.Windows.Forms.Label();
+            this.lbPT = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbContador = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbAfaRussian)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbContador);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lbPT);
+            this.groupBox1.Controls.Add(this.lbPronuncia);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pctbAfaRussian);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Cyan;
+            this.groupBox1.Location = new System.Drawing.Point(3, -3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 405);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(76, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Anterior";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(270, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Próximo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(171, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Mostrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pctbAfaRussian
+            // 
+            this.pctbAfaRussian.Image = global::AlfaBetoRusso.Properties.Resources._1;
+            this.pctbAfaRussian.Location = new System.Drawing.Point(50, 32);
+            this.pctbAfaRussian.Name = "pctbAfaRussian";
+            this.pctbAfaRussian.Size = new System.Drawing.Size(320, 213);
+            this.pctbAfaRussian.TabIndex = 3;
+            this.pctbAfaRussian.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Em PT: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pronuncia: ";
+            // 
+            // lbPronuncia
+            // 
+            this.lbPronuncia.AutoSize = true;
+            this.lbPronuncia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPronuncia.Location = new System.Drawing.Point(184, 270);
+            this.lbPronuncia.Name = "lbPronuncia";
+            this.lbPronuncia.Size = new System.Drawing.Size(16, 24);
+            this.lbPronuncia.TabIndex = 6;
+            this.lbPronuncia.Text = "-";
+            // 
+            // lbPT
+            // 
+            this.lbPT.AutoSize = true;
+            this.lbPT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPT.Location = new System.Drawing.Point(184, 309);
+            this.lbPT.Name = "lbPT";
+            this.lbPT.Size = new System.Drawing.Size(16, 24);
+            this.lbPT.TabIndex = 7;
+            this.lbPT.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(239, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Card: ";
+            // 
+            // lbContador
+            // 
+            this.lbContador.AutoSize = true;
+            this.lbContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbContador.Location = new System.Drawing.Point(305, 309);
+            this.lbContador.Name = "lbContador";
+            this.lbContador.Size = new System.Drawing.Size(16, 24);
+            this.lbContador.TabIndex = 9;
+            this.lbContador.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(443, 403);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alfabeto Russo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbAfaRussian)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pctbAfaRussian;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbPT;
+        private System.Windows.Forms.Label lbPronuncia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbContador;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlfaBetoRusso
+{
+    public partial class Form1 : Form
+    {
+        public static int contImagem = 0, idTranslate = 0;
+        public static string pronuncia = "";
+        public static string PT = "";
+        public static string[] ptVetor = new string[200];
+        public static string[] pronunciaVetor = new string[200];
+        public Form1()
+        {
+            InitializeComponent();
+            loadVertors();
+            changeImage(1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void  changeImage(int op)
+        {
+            if (op == 1)
+            {
+                if (contImagem == 33)
+                {
+                    contImagem = 0;
+                    pctbAfaRussian.ImageLocation = (@"img\33.png");
+                    idTranslate = 33;
+                    loadLabel(idTranslate);
+                }
+                contImagem = contImagem + 1;
+                idTranslate = contImagem;
+                loadLabel(idTranslate);
+            }
+            else
+            {
+                contImagem = contImagem - 1;
+                if (contImagem == 0)
+                {
+                    contImagem = 33;
+                    pctbAfaRussian.ImageLocation = (@"img\33.png");
+                    idTranslate = 33;
+                    loadLabel(idTranslate);
+                }
+                idTranslate = contImagem;
+                loadLabel(idTranslate);
+            }
+            
+            if (contImagem == 1)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\1.png");
+            }
+            if (contImagem == 2)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\2.png");
+            }
+            if (contImagem == 3)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\3.png");
+            }
+            if (contImagem == 4)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\4.png");
+            }
+            if (contImagem == 5)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\5.png");
+            }
+            if (contImagem == 6)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\6.png");
+            }
+            if (contImagem == 7)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\7.png");
+            }
+            if (contImagem == 8)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\8.png");
+            }
+            if (contImagem == 9)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\9.png");
+            }
+            if (contImagem == 10)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\10.png");
+            }
+            if (contImagem == 11)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\11.png");
+            }
+            if (contImagem == 12)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\12.png");
+            }
+            if (contImagem == 13)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\13.png");
+            }
+            if (contImagem == 14)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\14.png");
+            }
+            if (contImagem == 15)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\15.png");
+            }
+            if (contImagem == 16)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\16.png");
+            }
+            if (contImagem == 17)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\17.png");
+            }
+            if (contImagem == 18)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\18.png");
+            }
+            if (contImagem == 19)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\19.png");
+            }
+            if (contImagem == 20)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\20.png");
+            }
+            if (contImagem == 21)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\21.png");
+            }
+            if (contImagem == 22)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\22.png");
+            }
+            if (contImagem == 23)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\23.png");
+            }
+            if (contImagem == 24)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\24.png");
+            }
+            if (contImagem == 25)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\25.png");
+            }
+            if (contImagem == 26)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\26.png");
+            }
+            if (contImagem == 27)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\27.png");
+            }
+            if (contImagem == 28)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\28.png");
+            }
+            if (contImagem == 29)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\29.png");
+            }
+            if (contImagem == 30)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\30.png");
+            }
+            if (contImagem == 31)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\31.png");
+            }
+            if (contImagem == 32)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\32.png");
+            }
+            if (contImagem == 33)
+            {
+                pctbAfaRussian.ImageLocation = (@"img\33.png");
+            }
+            
+            lbContador.Text = "" + contImagem;
+            clearLabel();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            changeImage(1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (contImagem == 1)
+            {
+
+            }
+            changeImage(2);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lbPronuncia.Text = pronunciaVetor[idTranslate-1];
+            lbPT.Text = ptVetor[idTranslate-1];
+        }
+
+        public void clearLabel ()
+        {
+            lbPronuncia.Text = "-";
+            lbPT.Text = "-";
+        }
+
+        public void loadLabel(int id)
+        {
+
+        }
+
+        public void loadVertors()
+        {
+            ptVetor[0] = "A";
+            pronunciaVetor[0] = "A";
+            ptVetor[1] = "B";
+            pronunciaVetor[1] = "b";
+            ptVetor[2] = "V";
+            pronunciaVetor[2] = "V";
+            ptVetor[3] = "G";
+            pronunciaVetor[3] = "G";
+            ptVetor[4] = "D";
+            pronunciaVetor[4] = "D";
+            ptVetor[5] = "";
+            pronunciaVetor[5] = "A";
+        }
+    }
+}
